ed09e6c9ad3d615f6de44ec8fc49113941a626e3
charliedowler <charliedowler15@gmail.com>
2014-10-09 21:44:03 +0100

    tweaks (WIP)

--- cli.js
+++ cli.js
@@ -1,12 +1,42 @@
 #!/usr/bin/env node
 
+var chalk = require('chalk');
+var prompt = require('prompt');
 var argv = require('minimist')(process.argv.slice(2));
-var TravelHistory = require('./');
+var TravelHistory = require('node-oyster-history');
 
-if (!argv.u || !argv.p) {
-  throw new Error('Missing arguments u and or p. Check the README for usage instructions.');
+function printResult(err, history) {
+  if (err) throw new Error(err);
+
+  if (history.length > 0) {
+    // Print nice format
+    history.forEach(function(trip) {
+      console.log(trip.journey + ' => ' + trip.charge);
+    });
+  }
+  else {
+    console.log(chalk.red('\nNo oyster history available, if you\'ve travelled recently it may take 24 hours for it to become available.'));
+  }
+  process.exit(0);
 }
 
-TravelHistory(argv.u, argv.p, function(err, history) {
-  console.log(history);
-});
+if (argv.u && argv.p) {
+  TravelHistory(argv.u, argv.p, printResult);
+}
+else {
+  prompt.start();
+
+  prompt.get({
+    properties: {
+      username: {
+        required: true
+      },
+      password: {
+        hidden: true,
+        required: true
+      }
+    }
+  }, function (err, result) {
+    TravelHistory(result.username, result.password, printResult);
+  });
+}
